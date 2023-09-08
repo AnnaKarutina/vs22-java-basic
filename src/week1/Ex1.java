a34cba2115066ee75f115d485f57b19067b32130
Anna <anna.karutina@khk.ee>
2023-09-07 23:53:36 -0700

    week1 - Ex1

---
+++ src/week1/Ex1.java
@@ -0,0 +1,12 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name = reader.nextLine();
+        System.out.println(name);
+    }
+}
